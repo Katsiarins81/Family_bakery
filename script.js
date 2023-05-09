5f31c95a572905b705ef6582809a6c6e2c59892a
Katsiarins81 <101558997+Katsiarins81@users.noreply.github.com>
2023-05-09 17:35:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let btnOrder = document.querySelector('.btn-order')
+let btnAbout = document.querySelector('.btn-about')
+
+btnAbout.addEventListener('click',()=>{
+    alert('Вся информация доступна по номеру телефона')
+})
+
+btnOrder.addEventListener('click',()=>{
+    alert('Что бы оставить заказ заполните форму')
+})
